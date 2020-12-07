fc7b956616c76f16ad12fcf22a6eb3d93d7947b4
Andrew Wood <andrewwood90@hotmail.com>
2020-12-07 09:03:59 +0000

    submission

--- app/controller.py
+++ app/controller.py
@@ -13,14 +13,21 @@ def index():
 #     return render_template(name.html) 
 
 #@app.route('/add_players', methods = ['POST'])
-@app.route('/add_players', methods = ['POST'])
+@app.route('/add-players', methods = ['POST'])
 def add_players():
     # print(request.form)
     # return "done"
-    player_1_name = request.form["name"]
-    #player_2_name = request.form["name"]
+    player_1_name = request.form["name1"]
+    player_2_name = request.form["name2"]
     new_player_1 = Player(name = player_1_name,)
-    #new_player_2 = Player(name = player_2_name)
+    new_player_2 = Player(name = player_2_name)
     add_new_player(new_player_1)
-    #add_new_player(new_player_2)
+    add_new_player(new_player_2)
     return render_template('index.html', title = 'Home', players = players)
+
+
+# @app.route('/play-mutliplayer', methods = ['POST'])
+# def play_multiplayer(first_player, second_player):
+#     return play_janken(first_player, second_player)
+
+
